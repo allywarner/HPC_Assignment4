e8980e3d43e2856db50fd3929970feeaecbb6f4d
allywarner <allywarner@sci.utah.edu>
2016-04-22 15:52:30 -0600

    Starting over

--- main.cpp
+++ main.cpp
@@ -3,28 +3,46 @@
 //High Performance Computing - CS 6230
 //Bitonic Sort
 
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <ctime>
-#include <cstring>
 #include <mpi.h>
+#include <string.h>
 
 using namespace std;
+void SequentialSort(void);
+void CompareLow(int bit);
+void CompareHigh(int bit);
+int ComparisonFunc(const void * a, const void * b);
 
-//MAIN FUNCTION
+//Main Function
 //Inputs:
 int main(int argc, char* argv[]){
     
     //Initialize
     MPI_Init(&argc,&argv);
     
+    //Throws an error
+    if (argc < 2){
+        cerr << "Error. Please input the size of the array to be sorted.";
+        return 1;
+    }
+    
+    //Get the array's length to be sorted.
+    int arrayLength = atoi(argv[1]);
+    
     //Getting the rank and the size
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    
+    //Generate random array of the given length
+    int *array = new int[arrayLength];
+    for(int i = 0,i < arrayLength,i++){
+        array[i] = rand()*100+1;
+        cout << array[i] << endl;
+    }
     
     MPI_Finalize();
 }
